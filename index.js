505c379050fb2c61a417467868174737efcf15af
michya96 <michja144@gmail.com>
2022-09-20 01:26:03 +0200

    testing cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require('express');
-const Joi = require('joi');
+const { use } = require("./routes/users");
 const users = require('./routes/users')
 const port = process.env.PORT || 3000;
 app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+}))
 app.use('/api', users);
 app.listen(port, (req,res) => console.log(`server started on port ${port}`))
