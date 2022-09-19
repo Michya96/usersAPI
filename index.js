dc3976c768a11205ae49b791187e61c6ce31b6a4
michya96 <michja144@gmail.com>
2022-09-20 01:32:04 +0200

    fixing cors,

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const { use } = require("./routes/users");
 const users = require('./routes/users')
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 app = express();
 
 app.use(express.json());
 
 app.use(cors({
-    origin: "*",
+    origin: "127.0.0.1:5500",
 }))
 app.use('/api', users);
 app.listen(port, (req,res) => console.log(`server started on port ${port}`))
