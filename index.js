65a8b326e64a804433ab3fd149b6396ee5d6241d
michya96 <michja144@gmail.com>
2022-09-19 18:15:58 +0200

    Connecting to mongoDB

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const Joi = require('joi');
 const users = require('./routes/users')
@@ -7,3 +8,9 @@ app = express();
 app.use(express.json());
 app.use('/api', users);
 app.listen(port, (req,res) => console.log(`server started on port ${port}`))
+
+// Connecting to db
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
